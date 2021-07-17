ec0ec2d95c21d08bca3711b5459092992b1bc3fa
utgwkk <utagawakiki@gmail.com>
2021-07-17 11:02:00 +0900

    server: make file uploadable

--- server/src/graphql.ts
+++ server/src/graphql.ts
@@ -1,13 +1,74 @@
-import { GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
+import {
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from "graphql";
+import { FileUpload, GraphQLUpload } from "graphql-upload";
+
+type UploadedFile = {
+  filename: string;
+  length: number;
+};
+
+const uploadedFiles: UploadedFile[] = [];
+
+const FileType = new GraphQLObjectType<UploadedFile>({
+  name: "File",
+  fields: {
+    filename: {
+      type: GraphQLNonNull(GraphQLString),
+      resolve(parent) {
+        return parent.filename;
+      },
+    },
+    length: {
+      type: GraphQLNonNull(GraphQLInt),
+      resolve(parent) {
+        return parent.length;
+      },
+    },
+  },
+});
 
 export const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: "RootQueryType",
     fields: {
-      hello: {
-        type: GraphQLString,
+      files: {
+        type: GraphQLNonNull(GraphQLList(GraphQLNonNull(FileType))),
         resolve() {
-          return "world";
+          return uploadedFiles;
+        },
+      },
+    },
+  }),
+  mutation: new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+      uploadFile: {
+        type: FileType,
+        args: {
+          file: {
+            type: GraphQLNonNull(GraphQLUpload),
+          },
+        },
+        async resolve(parent, { file }) {
+          const { filename, createReadStream } =
+            await (file as Promise<FileUpload>);
+
+          let length = 0;
+          const stream = createReadStream();
+          for await (const chunk of stream) {
+            length += chunk.length;
+          }
+
+          const uploadedFile = { filename, length };
+          uploadedFiles.push(uploadedFile);
+
+          return uploadedFile;
         },
       },
     },
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
+import { graphqlUploadExpress } from "graphql-upload";
 
 import { schema } from "./graphql";
 
 const app = express();
-app.use("/api/graphql", graphqlHTTP({ schema, graphiql: true }));
+app.use(
+  "/api/graphql",
+  graphqlUploadExpress(),
+  graphqlHTTP({ schema, graphiql: true })
+);
 
 app.listen(4000);
