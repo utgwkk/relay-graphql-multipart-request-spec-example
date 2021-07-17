d971b73b8a581e6c94a0b94bcde93c034013a1b0
utgwkk <utagawakiki@gmail.com>
2021-07-17 11:18:17 +0900

    front: named export

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-function App() {
+export function App() {
   return (
     <div className="App">
       <h1>Hello, world!</h1>
     </div>
   );
 }
-
-export default App;
